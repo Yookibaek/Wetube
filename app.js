28f1bb10a28c276f724e33bddfde64a4a650e772
Yookibaek <psykibaek@gmail.com>
2019-03-13 23:02:12 +0900

    지역변수 추가
    - middlewares.js로 미들웨어함수 구현하여 해당함수안에서 지역변수 추가했음. 이 변수들은 pug에서 #{변수이름} 으로 사용가능한데 왜냐면 이 미들웨어가 더 이전에 실행되기때문임.
    -

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieparser from "cookie-parser";
 import bodyparser from "body-parser";
+import { localMiddleware } from "./middlewares";
 import userRouter from "./routers/userRouter" // default로 export하지않아서 이렇게됨
 import videoRouter from "./routers/videoRouter" // default로 export하지않아서 이렇게됨
 import globalRouter from "./routers/globalRouter"
@@ -12,11 +13,15 @@ import routes from "./routes"
 const app = express();
 
 // Middlewares
+app.use(helmet());      // 보안
+app.set("view engine","pug")
 app.use(cookieparser()) // 서버가 유저로부터 받은 쿠키 이해하는 방법
 app.use(bodyparser.urlencoded({extended :true}))   // 서버가 유저로부터 받은 바디 이해하는 방법
 app.use(bodyparser.json())   // 서버가 유저로부터 받은 바디 이해하는 방법
-app.use(helmet());      // 보안
 app.use(morgan("dev"))  // 접속자 로깅 기능
+app.use(localMiddleware)
+
+
 
 // Routers
 app.use(routes.home, globalRouter);      // /join,search,등을 다룸
--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,2 +1,7 @@
-export const home = (req,res)=> res.send("home")
+export const home = (req,res)=> res.render("home") // views폴더에서 제목이 home이고 확장자가 pug인놈을 찾아줌.
 export const search = (req,res) => res.send("search")
+export const videos = (req,res) => res.send("Videos")
+export const upload = (req,res) => res.send("Upload")
+export const videoDetail = (req,res) => res.send("Video Detail")
+export const editVideo = (req,res) => res.send("Edit Video")
+export const deleteVideo = (req,res) => res.send("Delete Video")
