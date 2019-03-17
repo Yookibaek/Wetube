f06fc0230b2b4d5bf1f80b48eba323231a1b4e77
Yookibaek <psykibaek@gmail.com>
2019-03-17 23:55:56 +0900

    mongodb setting complete

--- middlewares.js
+++ middlewares.js
@@ -1,6 +1,8 @@
 import routes from "./routes";
 
 export const localMiddleware = (req,res,next)=>{
+    // 여기있는 siteName, routes, user등의 객체들이
+    // 모두 response에서 접근가능한 변수들임.
 res.locals.siteName = "WeTube"
 res.locals.routes = routes;
 res.locals.user = {
